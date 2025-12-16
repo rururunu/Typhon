f04b060ef2625af410e0d4bcd25d068696ddd87b
runu <1032823838@qq.com>
2025-12-16 21:37:44 +0800

    init

---
+++ src/main/java/com/typhon/typhon/TyphonApplication.java
@@ -0,0 +1,13 @@
+package com.typhon.typhon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyphonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TyphonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/typhon/typhon/TyphonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typhon.typhon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyphonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
